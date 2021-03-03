b1895f16ca2a1a8251da2fe06ea2a26edc1261d4
Nafis <27172694+negarineh@users.noreply.github.com>
2021-03-04 01:04:00 +1100

    updated get-arnie-quotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,15 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const requests = urls.map(url => httpGet(url));
+
+  return Promise.all(requests)
+    .then(responses => responses.map(
+      response => response.status === 200 ?
+        { 'Arnie Quote': (JSON.parse(response.body)).message }
+        :
+        { 'FAILURE': (JSON.parse(response.body)).message }
+    ));
 };
 
 module.exports = {
